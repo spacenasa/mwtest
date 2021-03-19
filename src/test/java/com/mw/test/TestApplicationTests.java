b6c166c895dbef529e6ecb792f7c41398fae5784
wjh <spacenasa83@gmail.com>
2021-03-19 18:14:56 +0900

    init

---
+++ src/test/java/com/mw/test/TestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mw.test;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
